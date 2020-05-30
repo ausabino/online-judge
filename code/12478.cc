80da1a6c074095b10b3d982ac56991b0d169b3cc
Alan Sabino <alan.sabino@usp.br>
2020-05-30 16:46:27 -0300

    Adiciona caça-palavra

--- code/12478.cc
+++ code/12478.cc
@@ -2,4 +2,17 @@
 
 using namespace std;
 
-int main () { return 0; }
+int main ()
+{
+  const char grid[9][9] = {{'O', 'B', 'I', 'D', 'A', 'I', 'B', 'K', 'R'},
+                           {'R', 'K', 'A', 'U', 'L', 'H', 'I', 'S', 'P'},
+                           {'S', 'A', 'D', 'I', 'Y', 'A', 'N', 'N', 'O'},
+                           {'H', 'E', 'I', 'S', 'A', 'W', 'H', 'I', 'A'},
+                           {'I', 'R', 'A', 'K', 'I', 'B', 'U', 'L', 'S'},
+                           {'M', 'F', 'B', 'I', 'N', 'T', 'R', 'N', 'O'},
+                           {'U', 'T', 'O', 'Y', 'Z', 'I', 'F', 'A', 'H'},
+                           {'L', 'E', 'B', 'S', 'Y', 'N', 'U', 'N', 'E'},
+                           {'E', 'M', 'O', 'T', 'I', 'O', 'N', 'A', 'L'}};
+
+  return 0;
+}
